b72bef23a82aa35bb024854745ae5e9ad601771f
Maciej Treder <contact@maciejtreder.com>
2020-01-27 18:46:10 +0100

    step 14

--- rxjs/rx-http-request.js
+++ rxjs/rx-http-request.js
@@ -1,5 +1,6 @@
 import { RxHR } from '@akanass/rx-http-request';
 import { map, flatMap } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
 
 const BASE_PATH = `https://maciejtreder.github.io/asynchronous-javascript`;
 
@@ -14,5 +15,22 @@ const directorMovies$ = directorId$.pipe(
          return RxHR.get(`${BASE_PATH}/directors/${id}/movies`, {json: true}) 
     }),
     map(resp => resp.body)
- );
-directorMovies$.subscribe(console.log);
+);
+
+function getAverageScore(movie) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.rating;
+    return RxHR.get(`${BASE_PATH}/movies/${movie.id}/reviews`, {json: true}).pipe(
+        map(response => response.body.reduce(reducer, 0) / response.body.length),
+        map(response => { return {title: movie.title, averageScore: response}; })
+    );
+} 
+
+const moviesRatings$ = directorMovies$.pipe(
+    flatMap(movies => {
+        const observables$ = [];
+        movies.forEach(movie => observables$.push(getAverageScore(movie)));
+        return combineLatest(observables$);
+    })
+);
+
+moviesRatings$.subscribe(console.log);
